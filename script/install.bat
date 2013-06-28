9725ca0005aa9944fff388abd1469b588b3635d2
choi sung ki <florist.sk@gmail.com>
2013-06-28 14:00:16 +0900

    add sublime-settings file.

--- script/install.bat
+++ script/install.bat
@@ -7,4 +7,6 @@ setx HOME %homedrive%%homepath%
 mklink %homedrive%%homepath%\_vimrc %homedrive%%homepath%\dotfiles\vim\_vimrc
 mklink %homedrive%%homepath%\_gvimrc %homedrive%%homepath%\dotfiles\vim\_gvimrc
 
+@echo create sublime-settings symbolic link
+mklink "%appdata%\Sublime Text 2\Packages\User\Preferences.sublime-settings" %userprofile%\dotfiles\sublime\Preferences.sublime-settings
 pause
