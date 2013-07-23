fe03c87cc19bfe5ce12ab1609cbd83631e94c4b9
choi sung ki <florist.sk@gmail.com>
2013-07-23 16:36:45 +0900

    add .gitconfig file

--- script/setup.bat
+++ script/setup.bat
@@ -1,11 +1,14 @@
 @echo off
 
 @echo set home
-setx HOME %homedrive%%homepath%
+setx HOME %userprofile%
+
+@echo create gitconfig symbolic link
+mklink %userprofile%\.gitconfig %userprofile%\dotfiles\git\.gitconfig
 
 @echo create vimrc symbolic link
-mklink %homedrive%%homepath%\_vimrc %homedrive%%homepath%\dotfiles\vim\_vimrc
-mklink %homedrive%%homepath%\_gvimrc %homedrive%%homepath%\dotfiles\vim\_gvimrc
+mklink %userprofile%\_vimrc %userprofile%\dotfiles\vim\_vimrc
+mklink %userprofile%\_gvimrc %userprofile%\dotfiles\vim\_gvimrc
 
 @echo create sublime-settings symbolic link
 mklink "%appdata%\Sublime Text 2\Packages\User\Preferences.sublime-settings" %userprofile%\dotfiles\sublime\Preferences.sublime-settings
