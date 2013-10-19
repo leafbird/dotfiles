212880dc6a13082ed5dc21bac9875deac9770e7b
choi sung ki <florist.sk@gmail.com>
2013-10-19 23:49:59 +0900

    sublime 처음 설치시 존재하는 설정파일 제거하는 처리 추가.

--- script/setup.bat
+++ script/setup.bat
@@ -11,5 +11,6 @@ mklink %userprofile%\_vimrc %userprofile%\dotfiles\vim\_vimrc
 mklink %userprofile%\_gvimrc %userprofile%\dotfiles\vim\_gvimrc
 
 @echo create sublime-settings symbolic link
+if exist "%appdata%\Sublime Text 2\Packages\User\Preferences.sublime-settings" del "%appdata%\Sublime Text 2\Packages\User\Preferences.sublime-settings"
 mklink "%appdata%\Sublime Text 2\Packages\User\Preferences.sublime-settings" %userprofile%\dotfiles\sublime\Preferences.sublime-settings
 pause
