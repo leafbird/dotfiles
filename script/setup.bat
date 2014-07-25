d40fa1a242fa9833242e40bac7b743c1f4316cf3
leafbird <florist.sk@gmail.com>
2014-07-26 01:18:26 +0900

    블로그 관련 스크립팅 제거.

--- script/setup.bat
+++ script/setup.bat
@@ -11,8 +11,4 @@ mklink %userprofile%\_gvimrc %userprofile%\dotfiles\vim\_gvimrc
 if exist "%appdata%\Sublime Text 2\Packages\User\Preferences.sublime-settings" del "%appdata%\Sublime Text 2\Packages\User\Preferences.sublime-settings"
 mklink "%appdata%\Sublime Text 2\Packages\User\Preferences.sublime-settings" %userprofile%\dotfiles\sublime\Preferences.sublime-settings
 
-@echo crate blog commands
-rem robocopy %userprofile%\dotfiles\blog %userprofile%\blog /NJH /NDL
-rem mklink /J %userprofile%\blog %userprofile%\dotfiles\blog
-
 pause
