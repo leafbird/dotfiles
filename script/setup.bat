6e4e9027f49221873c62d6fa9b93e09006b99710
leafbird <florist.sk@gmail.com>
2017-03-10 15:54:48 +0900

    sublime3 setting added

--- script/setup.bat
+++ script/setup.bat
@@ -7,8 +7,12 @@ mklink %userprofile%\.gitconfig %userprofile%\dotfiles\git\.gitconfig
 mklink %userprofile%\_vimrc %userprofile%\dotfiles\vim\_vimrc
 mklink %userprofile%\_gvimrc %userprofile%\dotfiles\vim\_gvimrc
 
-@echo create sublime-settings symbolic link
+@echo create sublime2-settings symbolic link
 if exist "%appdata%\Sublime Text 2\Packages\User\Preferences.sublime-settings" del "%appdata%\Sublime Text 2\Packages\User\Preferences.sublime-settings"
 mklink "%appdata%\Sublime Text 2\Packages\User\Preferences.sublime-settings" %userprofile%\dotfiles\sublime\Preferences.sublime-settings
+
+@echo create sublime2-settings symbolic link
+if exist "%appdata%\Sublime Text 3\Packages\User\Preferences.sublime-settings" del "%appdata%\Sublime Text 3\Packages\User\Preferences.sublime-settings"
+mklink "%appdata%\Sublime Text 3\Packages\User\Preferences.sublime-settings" %userprofile%\dotfiles\sublime\Preferences.sublime-settings
 
 pause
