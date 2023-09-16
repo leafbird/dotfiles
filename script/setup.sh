9d0aa4b0e0c16030d0c8b328fce38f3f867a9428
leafbird <florist.sk@gmail.com>
2023-09-16 20:30:05 +0900

    chore: remove unused files

--- script/setup.sh
+++ script/setup.sh
@@ -4,11 +4,9 @@ rm ~/.profile
 rm ~/.bash_profile
 rm ~/.gitconfig
 rm ~/.vimrc
-rm ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Preferences.sublime-settings
 
 ln -s ~/dotfiles/osx/.profile ~/.profile
 ln -s ~/dotfiles/osx/.bash_profile ~/.bash_profile
 ln -s ~/dotfiles/git/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/vim/_vimrc ~/.vimrc
-ln -s ~/dotfiles/sublime/Preferences_mac.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Preferences.sublime-settings
 
