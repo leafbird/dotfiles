71f65fa9980e9e54338ccc72557b0cf05b254654
leafbird <florist.sk@gmail.com>
2023-10-02 19:38:28 +0900

    update: bash 설정 대신 zsh 설정 추가.(osx)

--- osx/.bash_profile
+++
@@ -1,5 +0,0 @@
-source ~/.profile
-export PS1="$C_CYAN\u:$C_YELLOW\w \$$C_DEFAULT "
-
-export CLICOLOR=1
-export LSCOLORS=DxFxCxGxBxegedabagaced
--- script/setup.sh
+++ script/setup.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-rm ~/.profile
-rm ~/.bash_profile
+rm ~/.zshrc
 rm ~/.gitconfig
 rm ~/.vimrc
 
-ln -s ~/dotfiles/osx/.profile ~/.profile
-ln -s ~/dotfiles/osx/.bash_profile ~/.bash_profile
+ln -s ~/dotfiles/osx/.zshrc ~/.zshrc
 ln -s ~/dotfiles/git/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/vim/_vimrc ~/.vimrc
 
