4e59fe78322238a43c098ae93be79167870086ee
leafbird <florist.sk@gmail.com>
2024-02-09 09:26:21 +0900

    windows의 nvim 설정 누락사항 추가

--- script/setup.bat
+++ script/setup.bat
@@ -10,5 +10,13 @@ mklink %userprofile%\_gvimrc %userprofile%\dotfiles\vim\_gvimrc
 
 pwsh ..\pwsh\setup.ps1
 
+set directory=%userprofile%\.config\nvim
+if not exist "%directory%" (
+    mkdir "%directory%"
+)
+
+@echo create init.vim symbolic link
+mklink %userprofile%\.config\nvim\init.vim %userprofile%\dotfiles\vim\nvim.init
+
 popd
 pause
