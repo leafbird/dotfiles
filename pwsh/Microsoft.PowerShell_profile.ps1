d0b48236240c3fb8886465e82b057184b8a7d449
choisungki <florist.sk@gmail.com>
2025-01-09 09:36:17 +0900

    Update Microsoft.PowerShell_profile.ps1
    - 파워쉘 프로필에 posh-git, psfzf 설정 추가.

--- pwsh/Microsoft.PowerShell_profile.ps1
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -2,3 +2,11 @@ oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\atomic.omp.json" | Invoke-E
 
 # 실행시간이 너무 길어서 제거
 # neofetch
+
+Import-Module posh-git
+
+Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
+Set-PsFzfOption -TabExpansion
+
+# replace 'Ctrl+t' and 'Ctrl+r' with your preferred bindings:
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
