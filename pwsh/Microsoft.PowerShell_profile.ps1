d4e21e3e920be6fc09aef42defbec41140907232
choisungki <florist.sk@gmail.com>
2025-01-11 09:01:00 +0900

    posh-git 모듈 설치 자동화

--- pwsh/Microsoft.PowerShell_profile.ps1
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -3,10 +3,14 @@ oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\atomic.omp.json" | Invoke-E
 # 실행시간이 너무 길어서 제거
 # neofetch
 
-Import-Module posh-git
-
 Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
 Set-PsFzfOption -TabExpansion
 
 # replace 'Ctrl+t' and 'Ctrl+r' with your preferred bindings:
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+# Ensure posh-git module is installed and loaded
+if (-not (Get-Module -ListAvailable -Name posh-git)) {
+    Install-Module -Name posh-git -Scope CurrentUser -Force
+}
+Import-Module -Name posh-git
