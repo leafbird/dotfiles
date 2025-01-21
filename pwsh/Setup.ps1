a44c928ccc9b8dd622b1a781c266485278d2ad91
choisungki <florist.sk@gmail.com>
2025-01-21 23:29:33 +0900

    파워쉘 프로필 백업 및 설정 제거, VSCode 프로필 심볼릭 링크 추가

--- pwsh/Setup.ps1
+++ pwsh/Setup.ps1
@@ -12,21 +12,12 @@ if (-Not (Test-Path -Path $sourcePath)) {
     return
 }
 
-Write-Output "파워쉘 설정을 시작합니다. profile path: $profile"
-
-# 백업할 프로필 파일의 경로와 백업 파일 경로를 설정합니다.
-$backupPath = "$PROFILE.bak"
-
-# 백업 파일이 이미 존재하면 덮어쓰지 않도록 합니다.
-if (-Not (Test-Path -Path $backupPath -PathType Leaf)) {
-    Copy-Item -Path $profile -Destination $backupPath
-    Write-Host "기존 파워쉘 프로필을 백업했습니다: $backupPath"
-} else {
-    Write-Host "백업된 프로필 파일이 이미 존재합니다: $backupPath"
-}
-
 # dotfiles repository의 프로필 파일로 심볼릭 링크를 설정합니다.
 New-Item -Path $profile -ItemType SymbolicLink -Value $sourcePath -Force
+
+# $profile의 파일 이름을 'Microsoft.VSCode_profile.ps1'으로 변경
+$vsCodeProfile = $profile -replace "Microsoft.PowerShell_profile.ps1", "Microsoft.VSCode_profile.ps1"
+New-Item -Path $vsCodeProfile -ItemType SymbolicLink -Value $sourcePath -Force
 
 Write-Host "새로운 파워쉘 프로필 심볼릭 링크를 설정했습니다."
 Write-Host "파워쉘을 다시 시작하여 새로운 프로필을 사용하세요."
