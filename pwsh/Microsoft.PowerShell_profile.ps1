fbee9686ed9f5e18e238c121a252e6681149db3d
choisungki <florist.sk@gmail.com>
2025-02-01 11:09:09 +0900

    Update Microsoft.PowerShell_profile.ps1

--- pwsh/Microsoft.PowerShell_profile.ps1
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -1,21 +1,39 @@
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\atomic.omp.json" | Invoke-Expression
 
-# 실행시간이 너무 길어서 제거
-# neofetch
+$script:enableFzf = $false
 
-# Ensure posh-git module is installed and loaded
-if (-not (Get-Module -ListAvailable -Name PsFzf)) {
-    Install-Module -Name PsFzf -Scope CurrentUser -Force
+function Enable-Fzf {
+    if ($script:enableFzf) {
+        Write-Host "Fzf가 이미 활성화되어 있습니다."
+        return
+    }
+
+    $script:enableFzf = $true
+
+    $script:stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
+
+    if (-not (Get-Module -ListAvailable -Name PsFzf)) {
+        Write-Host "PsFzf 모듈을 설치합니다..."
+        Install-Module -Name PsFzf -Scope CurrentUser -Force
+    }
+
+    Write-Host "탭 완성을 위한 설정을 추가합니다..."
+    Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
+    Set-PsFzfOption -TabExpansion
+
+    Write-Host "Ctrl+t / Ctrl+r 키 입력을 설정합니다..."
+    Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+    # Ensure posh-git module is installed and loaded
+    if (-not (Get-Module -ListAvailable -Name posh-git)) {
+        Write-Host "posh-git 모듈을 설치합니다..."
+        Install-Module -Name posh-git -Scope CurrentUser -Force
+    }
+
+    Write-Host "posh-git 모듈을 로드합니다..."
+    Import-Module -Name posh-git
+
+    Write-Host "Fzf 활성화가 완료되었습니다."
+    $script:stopwatch.Stop()
+    Write-Host "소요 시간: $($script:stopwatch.ElapsedMilliseconds)ms"
 }
-
-Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
-Set-PsFzfOption -TabExpansion
-
-# replace 'Ctrl+t' and 'Ctrl+r' with your preferred bindings:
-Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
-
-# Ensure posh-git module is installed and loaded
-if (-not (Get-Module -ListAvailable -Name posh-git)) {
-    Install-Module -Name posh-git -Scope CurrentUser -Force
-}
-Import-Module -Name posh-git
