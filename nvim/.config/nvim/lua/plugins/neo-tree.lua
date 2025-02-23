f69658c5caf2acfe3eaea6afa5915a72c986fa45
choisungki <florist.sk@gmail.com>
2025-02-23 21:48:22 +0900

    update nvim setting. show hidden file in neo-tree

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -6,5 +6,17 @@ return {
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
     -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
-  }
+  },
+  config = function()
+    -- show hidden files
+    require("neo-tree").setup({
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          hide_hidden = false
+        }
+      }
+    })
+  end
 }
