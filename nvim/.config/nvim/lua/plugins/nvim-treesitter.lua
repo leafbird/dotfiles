597f439e43969c23bff669db6ffa110c098a426e
choisungki <florist.sk@gmail.com>
2025-12-29 10:55:46 +0900

    update nvim setup & nvm(node version manager) configuration in .zshrc)

--- nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,14 +1,10 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
-  config = function()
-    local configs = require("nvim-treesitter.configs")
-
-    configs.setup({
-        ensure_installed = { "lua", "c_sharp", "html" },
-        sync_install = false,
-        highlight = { enable = true },
-        indent = { enable = true },  
-      })
-  end
+  opts = {
+    ensure_installed = { "lua", "c_sharp", "html" },
+    sync_install = false,
+    highlight = { enable = true },
+    indent = { enable = true },
+  },
 }
