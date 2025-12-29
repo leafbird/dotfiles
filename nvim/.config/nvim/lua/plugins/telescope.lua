6b2ff262628a197e1772629e6b61c7057e751f6e
choisungki <florist.sk@gmail.com>
2025-12-29 10:55:46 +0900

    update nvim setup & nvm(node version manager) configuration in .zshrc)

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -2,8 +2,10 @@ local mapKey = require("utils.keyMapper").mapKey
 return {
 	{
 		"nvim-telescope/telescope.nvim",
-		tag = "0.1.8",
-		dependencies = { "nvim-lua/plenary.nvim" },
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope-ui-select.nvim", -- Added as a dependency
+		},
 		config = function()
 			local builtin = require("telescope.builtin")
 			mapKey("<leader>ff", builtin.find_files)
@@ -11,8 +13,6 @@ return {
 			mapKey("<leader>fb", builtin.buffers)
 			mapKey("<leader>fh", builtin.help_tags)
 
-			-- from : https://github.com/nvim-telescope/telescope.nvim/wiki/Configuration-Recipes#file-and-text-search-in-hidden-files-and-directories
-			-- file and text search in hidden files and directories
 			local telescope = require("telescope")
 			local telescopeConfig = require("telescope.config")
 
@@ -25,31 +25,24 @@ return {
 			table.insert(vimgrep_arguments, "--glob")
 			table.insert(vimgrep_arguments, "!**/.git/*")
 
+            -- MERGED SETUP CALL
 			telescope.setup({
 				defaults = {
-					-- `hidden = true` is not supported in text grep commands.
 					vimgrep_arguments = vimgrep_arguments,
 				},
 				pickers = {
 					find_files = {
-						-- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
 						find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
 					},
 				},
-			})
-		end,
-	},
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-		config = function()
-			local telescope = require("telescope")
-			telescope.setup({
 				extensions = {
 					["ui-select"] = {
 						require("telescope.themes").get_dropdown({}),
 					},
 				},
 			})
+
+            -- LOAD EXTENSION
 			telescope.load_extension("ui-select")
 		end,
 	},
