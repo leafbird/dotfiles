15aae78b673a59dec4ba35fe99e83254bc07cfba
choisungki <florist.sk@gmail.com>
2026-05-05 16:12:43 +0900

    update nvim configurations

--- nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,10 +1,13 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "main",
   build = ":TSUpdate",
-  opts = {
-    ensure_installed = { "lua", "c_sharp", "html" },
-    sync_install = false,
-    highlight = { enable = true },
-    indent = { enable = true },
-  },
+  lazy = false,
+  config = function()
+    require("nvim-treesitter").install({ "lua", "c_sharp", "html" })
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = { "lua", "cs", "html" },
+      callback = function() vim.treesitter.start() end,
+    })
+  end,
 }
